18ec0cf6d2ba49cce4453d6b23cfe006a27c32b4
Steve Heindel <stevenheindel@gmail.com>
2020-02-19 18:54:47 -0500

    Mandelbrot works but is a bit slow

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+mod timer;
 mod utils;
 
 use wasm_bindgen::prelude::*;
@@ -9,11 +10,92 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+pub struct Mandelbrot {
+    view_left: f32, // Left side of the view in complex space
+    view_top: f32, // Top side of the view in complex space
+    view_width: f32, // Width of the view in complex space (left + width = right)
+    view_height: f32, // Height of the view in complex space (top + height = bottom)
+    max_iterations: u8,
+    plot_width: u32,
+    plot_height: u32,
+    plot: Vec<u8>, // The plot data. Must be sized at least (width * height) elements.
 }
 
+/// Private methods, not visible to JavaScript
+impl Mandelbrot {
+    // Get the index into plot that corresponds to the given row (y) and col (x)
+    fn get_index(&self, x: u32, y: u32) -> usize {
+        (y * self.plot_width + x) as usize
+    }
+
+    fn iterate(&self, x: u32, y: u32) -> Option<u8> {
+        let cx = self.view_left + (x as f32) / ((self.plot_width - 1) as f32) * self.view_width;
+        let cy = self.view_top + (y as f32) / ((self.plot_height - 1) as f32) * self.view_height;
+
+        let mut zx = cx;
+        let mut zy = cy;
+        
+        for i in 0..self.max_iterations {
+            let xi = zx * zx - zy * zy + cx;
+            let yi = 2.0 * zx * zy + cy;
+
+            if (xi * xi + yi * yi) > 4.0 { // Guaranteed to escape
+                return Some(i);
+            }
+
+            zx = xi;
+            zy = yi;
+        }
+
+        None
+    }
+}
+
+/// Public methods, exported to JavaScript.
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, mandelbrot-wasm!");
+impl Mandelbrot {
+    pub fn new(plot_width: u32, plot_height: u32) -> Self {
+        utils::set_panic_hook();
+
+        let plot = vec![0; (plot_width * plot_height) as usize];
+
+        Self {
+            view_left: -2.5,
+            view_top: -2.0,
+            view_width: 4.0,
+            view_height: 4.0,
+            max_iterations: 6,
+            plot_width: plot_width,
+            plot_height: plot_height,
+            plot: plot,
+        }
+    }
+
+    pub fn max_iterations(&self) -> u8 {
+        self.max_iterations
+    }
+
+    pub fn plot_width(&self) -> u32 {
+        self.plot_width
+    }
+
+    pub fn plot_height(&self) -> u32 {
+        self.plot_height
+    }
+
+    pub fn plot_generate(&mut self) {
+        let _timer = timer::Timer::new("Mandelbrot::plot_generate()");
+
+        for x in 0..self.plot_width {
+            for y in 0..self.plot_height {
+                let iterations = self.iterate(x, y);
+                let idx = self.get_index(x, y);
+                self.plot[idx] = iterations.unwrap_or(self.max_iterations);
+            }
+        }
+    }
+
+    pub fn plot_data(&self) -> *const u8 {
+        self.plot.as_ptr()
+    }
 }
--- www/index.js
+++ www/index.js
@@ -1,3 +1,109 @@
-import * as wasm from "hello-wasm-pack";
+import { Mandelbrot } from "mandelbrot_wasm";
+import { memory } from "mandelbrot_wasm/mandelbrot_wasm_bg"; // Import the WebAssembly memory at the top of the file.
 
-wasm.greet();
+// Construct the plot generator
+const width = 500;
+const height = 500;
+const mandelbrot = Mandelbrot.new(width, height);
+
+// Give the canvas room for our plot
+const canvas = document.getElementById("mandelbrot-canvas");
+canvas.width = width;
+canvas.height = height;
+
+const ctx = canvas.getContext('2d');
+
+const renderLoop = () => {
+    fps.render();
+    //drawPlot();
+    requestAnimationFrame(renderLoop);
+};
+
+const getIndex = (row, column) => {
+    return row * width + column;
+};
+
+const rgbToHex = (rgb) => {
+    var hex = Number(rgb).toString(16);
+    if (hex.length < 2) {
+        hex = "0" + hex;
+    }
+    return hex;
+}
+
+const getRGB = (value) => {
+    const r = ((value & 0x03) >> 0) * (256 / 4);
+    const g = ((value & 0x0C) >> 2) * (256 / 4);
+    const b = ((value & 0x30) >> 4) * (256 / 4);
+    return "#" + rgbToHex(r) + rgbToHex(g) + rgbToHex(b);
+};
+
+const drawPlot = () => {
+    const plotPtr = mandelbrot.plot_data();
+    const plot = new Uint8Array(memory.buffer, plotPtr, width * height);
+
+    // var myImageData = ctx.createImageData(width, height);
+    // Uint8ClampedArray.from(plot);
+
+    for (let row = 0; row < height; row++) {
+        for (let col = 0; col < width; col++) {
+            const idx = getIndex(row, col);
+            var rgb;
+            if (plot[idx] >= mandelbrot.max_iterations()) {
+                rgb = "#000000";
+            } else {
+                rgb = getRGB(plot[idx]);
+            }
+            ctx.fillStyle = rgb;
+
+            ctx.fillRect(col, row, 1, 1);
+        }
+    }
+};
+
+const fps = new class {
+    constructor() {
+        this.fps = document.getElementById("fps");
+        this.frames = [];
+        this.lastFrameTimeStamp = performance.now();
+    }
+
+    render() {
+        // Convert the delta time since the last frame render into a measure
+        // of frames per second.
+        const now = performance.now();
+        const delta = now - this.lastFrameTimeStamp;
+        this.lastFrameTimeStamp = now;
+        const fps = 1 / delta * 1000;
+
+        // Save only the latest 100 timings.
+        this.frames.push(fps);
+        if (this.frames.length > 100) {
+            this.frames.shift();
+        }
+
+        // Find the max, min, and mean of our 100 latest timings.
+        let min = Infinity;
+        let max = -Infinity;
+        let sum = 0;
+        for (let i = 0; i < this.frames.length; i++) {
+            sum += this.frames[i];
+            min = Math.min(this.frames[i], min);
+            max = Math.max(this.frames[i], max);
+        }
+        let mean = sum / this.frames.length;
+
+        // Render the statistics.
+        this.fps.textContent = `
+Frames per Second:
+latest = ${Math.round(fps)}
+avg of last 100 = ${Math.round(mean)}
+min of last 100 = ${Math.round(min)}
+max of last 100 = ${Math.round(max)}
+`.trim();
+    }
+};
+
+mandelbrot.plot_generate();
+drawPlot();
+requestAnimationFrame(renderLoop);
